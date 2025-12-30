0cc12e3bbb7d55ccd2e69f8a879ef309bc805c3c
janu <janup3675@gmail.com>
2025-12-30 08:54:18 +0530

    Fix Laravel entry point in public/index.php and update Controller.php

--- public/index.php
+++ public/index.php
@@ -1,37 +1,42 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// If using the PHP built-in server, serve the requested file directly when it exists
-if (php_sapi_name() === 'cli-server') {
-    $url = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__.$url['path'];
-    if (is_file($file)) {
-        return false;
-    }
-}
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap the application
-/** @var Application $app */
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| using the application's HTTP kernel. Then, we will send the
+| response back to this client's browser, allowing them to enjoy
+| our application.
+|
+*/
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Run the application (HTTP Kernel)
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Request::capture()
-);
+$request = Request::capture();
+
+$response = $kernel->handle($request);
 
 $response->send();
 
